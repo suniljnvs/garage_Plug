d162e1c4c43f35969129329be8c01cb7d75f0595
suniljnvs <sunikumar0551998@gmail.com>
2022-12-15 23:36:03 +0530

    done

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,3 +1,4 @@
+let mongoose = require('mongoose');
 const { model } = require("mongoose");
 let userModel = require("../models/userModel");
 let orderModel = require("../models/orderModel");
@@ -25,55 +26,53 @@ let createUser = async function (req, res) {
 }
 
 
-const create_Order = async function(req,res){
+const create_Order = async function (req, res) {
     try {
         const order = req.body;
 
-        const oderData = await orderModel.create (order);
-        res.status(201).send({ status: true, message: "Order created successfully", data:oderData  })
-        
+        const oderData = await orderModel.create(order);
+        res.status(201).send({ status: true, message: "Order created successfully", data: oderData })
+
     } catch (error) {
         res.status(500).send({ status: false, message: error.message })
     }
 }
 
 
-const calculateDiscount = async function (req, res){
+const calculateDiscount = async function (req, res) {
     try {
-        const user_id = req.body;
+        const user_id = req.body.userId;
         const totalPrice = req.body.totalPrice;
-        const id = user_id.userId
-        if(!user_id){
+
+        if (!user_id) {
             throw new Error("user_id is required")
         };
 
-        let data = await orderModel.find({userId : id})
-        // console.log(data.length)
-        // return false
-        let gold1 = "gold";
-        let platinum1 = "platinum"
-        if(data.length>=10 && data.length<20){
-            let caregorydata = await userModel.findOneAndUpdate({ _id: id }, { $set: { category : gold1 } }, { new: true })
+        let data = await orderModel.find({ userId: user_id })
+
+        if (data.length >= 10 && data.length < 20) {
+            let caregorydata = await userModel.findOneAndUpdate({ _id: user_id }, { $set: { category: "gold" } }, { new: true })
+
             let dis_percent = "10"
-            let discount = ((totalPrice * dis_percent)/100);
-            let payble_amount =  totalPrice - discount
+            let discount = ((totalPrice * dis_percent) / 100);
+            let payble_amount = totalPrice - discount
 
             const data1 = {};
-            data1.category="gold"
+            data1.category = "gold"
             data1.totalPrice = totalPrice;
             data1.discount = discount;
             data1.payble_amount = payble_amount;
 
-        console.log(data1)
+            console.log(data1)
 
             return data1;
 
         }
-        else if(data.length>20){
-            let caregorydata = await userModel.findOneAndUpdate({ _id: id }, { $set: { category : platinum1 } }, { new: true });
+        else if (data.length > 20) {
+            let caregorydata = await userModel.findOneAndUpdate({ _id: id }, { $set: { category: "platinum" } }, { new: true });
             let dis_percent = "20"
-            let discount = ((totalPrice * dis_percent)/100);
-            let payble_amount =  totalPrice - discount
+            let discount = ((totalPrice * dis_percent) / 100);
+            let payble_amount = totalPrice - discount
 
             const data1 = {};
             data1.category = "platinum"
@@ -81,7 +80,7 @@ const calculateDiscount = async function (req, res){
             data1.discount = discount;
             data1.payble_amount = payble_amount;
 
-        console.log(data1)
+            console.log(data1)
 
             return data1;
         };
@@ -94,11 +93,53 @@ const calculateDiscount = async function (req, res){
 
         console.log(data1)
         return data1
-        
+
     } catch (error) {
         res.status(500).send({ status: false, message: error.message })
     }
 }
 
 
-module.exports = { createUser,create_Order ,calculateDiscount}
+const check_Total_Discount = async function (req, res) {
+    try {
+        const user_id = req.body.userId;
+
+        let data = await orderModel.find({ userId: user_id });
+
+        if (data.length > 9) {
+
+            let data1 = await orderModel.find({ userId: user_id }).skip(9).limit(19);
+            // console.log(data1);
+            let sum1 = 0;
+            data1.forEach(element => {
+                sum1 += element.price;
+            });
+            // console.log(sum1);
+            var dis_percent = "10"
+            discount1 = ((sum1 * dis_percent) / 100);
+
+            let data2 = await orderModel.find({ userId: user_id }).skip(19);
+            let sum2 = 0;
+            data2.forEach(element => {
+                sum2 += element.price;
+            });
+            // console.log(sum2);
+            var dis_percent = "20"
+            discount2 = ((sum2 * dis_percent) / 100);
+            
+            total_discount = discount1 + discount2
+
+            console.log(total_discount);
+            return total_discount
+
+        } else {
+            (res.send({ status: true, message: "This is regular user", discount: ["0.00"] }));
+        }
+
+    } catch (error) {
+        res.status(500).send({ status: false, message: error.message })
+    }
+}
+
+
+module.exports = { createUser, create_Order, calculateDiscount, check_Total_Discount }
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,14 +3,17 @@ const express = require('express');
 const router = express.Router();
 
 
-const { createUser,create_Order,calculateDiscount } = require('../controllers/controller');
+const { createUser,create_Order,calculateDiscount,check_Total_Discount } = require('../controllers/controller');
 
 
 
-router.post("/create-user", createUser)
-// router.post("/login-user", login_User)
-router.post("/create-order", create_Order)
-router.post("/calculate-discount", calculateDiscount)
+router.post("/create-user", createUser);
+
+router.post("/create-order", create_Order);
+
+router.post("/calculate-discount", calculateDiscount);
+
+router.post("/total-discount", check_Total_Discount);
 
 
 
